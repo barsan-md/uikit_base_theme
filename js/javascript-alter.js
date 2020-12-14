22bfbf62b41d59b5ec060aba434405b149eb9b0d
barsan-md <barsan.md@gmail.com>
2020-12-14 19:37:05 +0100

    Initial commit

---
+++ js/javascript-alter.js
@@ -0,0 +1,5 @@
+Drupal.theme.tableDragToggle = () => `
+<div class="tabledrag-toggle-weight-wrapper" data-drupal-selector="tabledrag-toggle-weight-wrapper">
+  <button type="button" class="link tabledrag-toggle-weight uk-button uk-button-secondary uk-button-small" data-drupal-selector="tabledrag-toggle-weight"></button>
+</div>
+`
